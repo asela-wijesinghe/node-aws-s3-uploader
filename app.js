c6da24867c4a850196da34aa8be4e50c71609ff7
asela <123azeela@gmail.com>
2018-04-17 23:41:39 +0530

    setting changed on aws

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./config.json');
+
+app.use(
+	'/s3',
+	require('react-dropzone-s3-uploader/s3router')({
+		bucket: 'watchitlk-videos', // required,
+		region: 'ap-south-1',
+		headers: { 'Access-Control-Allow-Origin': '*' },
+		ACL: 'public-read'
+	})
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,14 +31,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(
-	'/s3',
-	require('react-dropzone-s3-uploader/s3router')({
-		bucket: 'watchitlk-videos' // required,
-    region: 'ap-south-1'
-	})
-);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
